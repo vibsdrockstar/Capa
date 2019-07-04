d81edaf186435d612daa515f1a879de4de4ebd86
Vibhor <bijoy.vibhor19@gmail.com>
2019-07-04 06:29:03 +0530

    Create Homepage.java

---
+++ Homepage.java
@@ -0,0 +1,32 @@
+package com.example.parker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+    public void openInfo(View view) {
+        switch (view.getId()) {
+            case R.id.b_hist:
+                startActivity(new Intent(this, AboutUs.class));
+                break;
+            case R.id.b_stat:
+                startActivity(new Intent(this, SuccessBooking.class));
+                break;
+            case R.id.b_feature:
+                startActivity(new Intent(this, ADMIN.class));
+                break;
+            case R.id.b_usertype:
+                startActivity(new Intent(this, UserType.class));
+                break;
+        }
+    }
+}
